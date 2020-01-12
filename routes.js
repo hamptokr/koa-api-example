fb41fa8ab62d3773b89f62f8397ce187c35113d2
Kramer Hampton <me@kramerhampton.dev>
2020-01-11 19:22:33 -0500

    add basic login form

--- routes.js
+++ routes.js
@@ -1,47 +1,59 @@
-const Router = require('koa-router')
-const Employee = require('./models').Employee
-const jwt = require('jsonwebtoken')
+const Router = require("koa-router");
+const Employee = require("./models").Employee;
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
-const BASE_URL = '/api'
-const SECRET = process.env.SECRET
-const router = new Router()
+const BASE_URL = "/api";
+const SECRET = process.env.SECRET;
+const router = new Router();
 
-router.post(`${BASE_URL}/login`, async (ctx) => {
-    const reqUsername = ctx.request.body.username
-    const reqPassword = ctx.request.body.password
+router.get("/login", async ctx => {
+  ctx.type = "html";
+  ctx.body = fs.createReadStream("login.html");
+});
 
-    const unauthorized = (ctx) => {
-        ctx.status = 401
-        ctx.body = {
-            error: 'Invalid username or password'
-        }
+// /api/login
+router.post(`${BASE_URL}/login`, async ctx => {
+  console.log(ctx.request.body);
+  const reqUsername = ctx.request.body.username;
+  const reqPassword = ctx.request.body.password;
+
+  const unauthorized = ctx => {
+    ctx.status = 401;
+    ctx.body = {
+      error: "Invalid username or password"
+    };
+  };
+
+  let attemptingUser;
+  try {
+    attemptingUser = await Employee.findOne({
+      where: { username: reqUsername }
+    });
+    if (attemptingUser != null && attemptingUser.password === reqPassword) {
+      ctx.status = 200;
+      ctx.body = {
+        username: attemptingUser.username,
+        given_name: attemptingUser.given_name,
+        family_name: attemptingUser.family_name,
+        role: attemptingUser.role,
+        created_at: attemptingUser.createdAt,
+        updated_at: attemptingUser.updatedAt
+      };
+
+      const token = jwt.sign(
+        { username: attemptingUser.username, role: attemptingUser.role },
+        SECRET
+      );
+      ctx.set("X-Auth", token);
+    } else {
+      unauthorized(ctx);
     }
+  } catch (err) {
+    console.error(err);
+    console.error(`Failed to find username: ${reqUsername}`);
+    unauthorized(ctx);
+  }
+});
 
-    let attemptingUser
-    try {
-        attemptingUser = await Employee.findOne({ where: { username: reqUsername }})
-        if (attemptingUser != null && attemptingUser.password === reqPassword) {
-            ctx.status = 200
-            ctx.body = {
-                username: attemptingUser.username,
-                given_name: attemptingUser.given_name,
-                family_name: attemptingUser.family_name,
-                role: attemptingUser.role,
-                created_at: attemptingUser.createdAt,
-                updated_at: attemptingUser.updatedAt,
-            }
-
-            const token = jwt.sign({ username: attemptingUser.username, role: attemptingUser.role }, SECRET)
-            ctx.set("X-Auth", token)
-        } else {
-            unauthorized(ctx)
-        }
-        
-    } catch(err) {
-        console.error(err)
-        console.error(`Failed to find username: ${reqUsername}`)
-        unauthorized(ctx)
-    }
-})
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // Load environment variables from .env
-require('dotenv').config()
-const Koa = require('koa')
-const Router = require('koa-router')
-const bodyParser = require('koa-bodyparser')
-const baseRoutes = require('./routes')
+require("dotenv").config();
+const Koa = require("koa");
+const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+const baseRoutes = require("./routes");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
-const app = new Koa()
+const app = new Koa();
 
-app.use(bodyParser())
-app.use(baseRoutes.routes())
+app.use(bodyParser());
+app.use(baseRoutes.routes());
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
-})
-
+  console.log(`Server listening on ${PORT}`);
+});
